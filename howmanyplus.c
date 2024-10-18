b1310b7cc8541cb0f28bdc347f7102cdcfd8fcd0
Sora700 <t236007@st.yamagata-u.ac.jp>
2024-10-18 19:16:55 +0900

    Add files via upload

---
+++ howmanyplus.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int k,s,t;
+    printf("How many fruits?");
+    scanf("%d", &k);
+    printf("How many fruits?");
+    scanf("%d", &s);
+    t = k + s;
+    printf("The number of fruits is %d", t);
+
+    return 0;
+}
